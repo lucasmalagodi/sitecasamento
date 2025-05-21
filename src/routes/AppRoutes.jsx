9fe7f9b4355a922705459bed1cbdcf3aaf7dd2f2
Lucas Malagodi <lucas.malagodi@outlook.com>
2025-05-20 21:41:09 -0300

    atualizando imagens e loading site

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
 import Home from "../pages/Home";
 import Recepcao from "../pages/Recepcao"; // Recepcao
 import { Presenca } from "../pages/Presenca";
@@ -6,22 +7,25 @@ import Lista from "../pages/Lista";
 // import NotFound from "../pages/NotFound";
 import Layout from "../components/Layout";
 import ScrollToTop from "../components/ScrollToTop";
+import Loading from "../components/Loading";
 
 const AppRoutes = () => {
   return (
     <Router>
       <ScrollToTop />
-      <Routes>
-        {/* Página Home SEM Header e Footer */}
-        <Route path="/" element={<Home />} />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          {/* Página Home SEM Header e Footer */}
+          <Route path="/" element={<Home />} />
 
-        {/* Agrupando as páginas que precisam do Layout */}
-        <Route element={<Layout />}>
-          <Route path="/recepcao" element={<Recepcao />} />
-          <Route path="/presenca" element={<Presenca />} />
-          <Route path="/lista" element={<Lista />} />
-        </Route>
-      </Routes>
+          {/* Agrupando as páginas que precisam do Layout */}
+          <Route element={<Layout />}>
+            <Route path="/recepcao" element={<Recepcao />} />
+            <Route path="/presenca" element={<Presenca />} />
+            <Route path="/lista" element={<Lista />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
